cb012ec9de6391ae84684ff9d77dd73de31a5262
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-04-09 19:42:45 +0300

    remade javascript to use class method

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
-let countDownDate = new Date("Apr 11, 2022 18:00:00 GMT").getTime();
-
-let updater = setInterval(function(){
-
-let currentTime = new Date().getTime();
-let timeLeft = countDownDate -currentTime;
-
-let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-document.querySelector("#days").innerHTML = days + "d"
-document.querySelector("#hours").innerHTML = hours + "h"
-document.querySelector("#mins").innerHTML = minutes + "m"
-document.querySelector("#secs").innerHTML = seconds + "s"
-
-if (timeLeft < -7200000){
-    clearInterval(updater);
-    document.querySelector("#days").innerHTML = ""
-    document.querySelector("#hours").innerHTML = "" 
-    document.querySelector("#mins").innerHTML = ""
-    document.querySelector("#secs").innerHTML = ""
-    document.querySelector("#end").innerHTML = "Meeting over! Resetting timer shortly";
-} else if (timeLeft < 0) {
-    clearInterval(updater);
-    document.querySelector("#days").innerHTML = ""
-    document.querySelector("#hours").innerHTML = "" 
-    document.querySelector("#mins").innerHTML = ""
-    document.querySelector("#secs").innerHTML = ""
-    document.querySelector("#end").innerHTML = "Meeting Time";
+class CountdownClock{
+    constructor(date){
+        this.date = date
+    }
+    countDown(){
+        let meetingTime = new Date(this.date).getTime();
+        let currentTime = new Date().getTime()
+        let timeLeft = meetingTime - currentTime;
+        let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
+    }
 }
-}, 1000)
 
-//Next meeting date
+let clock1EU = new CountdownClock("Apr 11, 2022 18:00:00 GMT");
+let clock2EU = new CountdownClock("Apr 19, 2022 12:00:00 GMT");
+let clock3EU = new CountdownClock("Apr 28, 2022 20:00:00 GMT");
 
+function time () {
+document.querySelector("#clock1").innerHTML = clock1EU.countDown();
+document.querySelector("#clock2").innerHTML = clock2EU.countDown();
+document.querySelector("#clock3").innerHTML = clock3EU.countDown();
+}
 
-//Meeting date after that
-
+let timeInterval = setInterval(time, 1000); //refresh every second
